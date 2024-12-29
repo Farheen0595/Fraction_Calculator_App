b8a8f4fa28e666fbe774b1b38cfbe6200e03737e
Farheen Syed <102013740+Farheen0595@users.noreply.github.com>
2024-12-29 15:08:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
 
     # Input for P = n/d and Q = n/d in one line
     st.write("### Enter Fractions:")
-    col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns([0.5, 1, 0.3, 1, 0.5, 0.5, 1, 0.3, 1])
+    col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns([0.5, 1, 0.3, 1, 0.5, 0.5, 0.5, 0.3, 1])
     with col1:
         st.write("P = ")
     with col2:
